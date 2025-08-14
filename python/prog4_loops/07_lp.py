d6b8a76f623adb9b836141e7fedecab80d956338
koushik-1410 <koushikosh2019@gmail.com>
2025-08-14 17:21:07 +0530

    Add files via upload

---
+++ python/prog4_loops/07_lp.py
@@ -0,0 +1,7 @@
+while True:
+    user_input=int(input("Enter a number btw 1 to 10:"))
+    if 1<= user_input <=10:
+        print("Game over")
+        exit()
+    else:
+        print("Retry!!")    
