b2badc9152996a473f01438ef3b5a3ab8a722c03
koushik-1410 <koushikosh2019@gmail.com>
2025-08-15 14:48:07 +0530

    Add files via upload

---
+++ DSA (cpp)/Lecture4/02_vowelchecker.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cout<<"Enter a charecter: ";
+    cin>>a;
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+        cout<<"vowel";
+    else
+    cout<<"consonent";
+}
